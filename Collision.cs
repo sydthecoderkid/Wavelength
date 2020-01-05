0ceca41f386145c2b9dee4b0ee629d89c3d39d22
Sydney Keating <bookmasterdc@gmail.com>
2020-01-04 23:08:47 -0500

    First commit

---
+++ Collision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public BoxCollider2D col;
+    public static float currentposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentposition = WalkingScript.position;
+    }
+   
+}
---
+++ WalkingScript.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingScript : MonoBehaviour
+{
+    public Animation walkanim;
+    public Animator walk;
+    public GameObject character;
+    public static float position;
+    private static float walkingposition;
+    private static Boolean facingleft;
+    private static Boolean facingright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkanim = this.gameObject.GetComponent<Animation>();
+        walk = this.gameObject.GetComponent<Animator>();
+       position = character.transform.position.x;
+        character = this.gameObject;
+        facingleft = false;
+        facingright = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector2(position, this.transform.position.y);
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+
+            MovingRight();
+            facingright = true;
+            facingleft = false;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+
+            MovingLeft();
+            facingright = false;
+            facingleft = true;
+        }
+
+        if (!(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
+        {
+
+            NotWalking();
+        }
+    }
+
+    private void MovingLeft()
+    {
+        walk.Play("Backwardwalk");
+        if (position >= -1.396643)
+        {
+            position -= Time.deltaTime / 2;
+        }
+    }
+
+    private void NotWalking()
+    {
+        if (facingright)
+        {
+            walk.Play("Idle");
+        }
+
+        if (facingleft)
+        {
+            walk.Play("IdleLeft");
+        }
+    }
+
+    private void MovingRight()
+    {
+        walk.Play("WalkAnim");
+        position += Time.deltaTime/2;
+    }
+}

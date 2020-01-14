698b09cdcf941ed465e1eefe0078c620ab565bff
Sydney Keating <bookmasterdc@gmail.com>
2020-01-13 21:30:28 -0500

    Added a dialog script

--- SpawnCharacter.cs
+++ SpawnCharacter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnCharacter : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class SpawnCharacter : MonoBehaviour
     public static bool firstspawn = true;
     public GameObject character;
     WalkingScript characterpos;
+
+    private Scene scene;
+    private string currentscene;
+
+    
+        public GameObject npc1;
+         public GameObject npc2;
+          public GameObject npc3;
     void Start()
     {
         //When testing combat systems, make sure to change firstspawn position.
@@ -19,7 +28,10 @@ public class SpawnCharacter : MonoBehaviour
             WalkingScript.position = -0.6f;
             firstspawn = false;
         }
+         scene = SceneManager.GetActiveScene();
+        currentscene = scene.name;
         Spawncharacter();
+        SpawnNPCS();
         
     }
 
@@ -34,13 +46,17 @@ public class SpawnCharacter : MonoBehaviour
         if (WalkingScript.switchedleft)
         {
             WalkingScript.position = 1f;
-            Debug.Log("Here-faceleft");
         }
          if (WalkingScript.switchedright)
         {
              WalkingScript.position = -0.8f;
-            Debug.Log("Here-faceright");
         }
+    }
+
+    public void SpawnNPCS(){
+        if(currentscene.Equals("Kitchen")){
+        }
+
     }
 
 
